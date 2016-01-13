5ec3c9c03ad369e783f3ca2a8c29383c825fef08
Smilent <leeyeah93@gmail.com>
2016-01-13 21:45:54 +0800

    Add scan

--- dirscan.py
+++ dirscan.py
@@ -2,14 +2,25 @@
 #coding:utf-8
 
 import sys
-import Queue
 import optparse
 import requests
 import threading
-import urlparse
+import time
 
+
+try:
+    import queue as Queue
+except Exception as e:
+    import Queue
+
+try:
+    from urllib import parse as urlparse
+except Exception as e:
+    import urlparse
 
 EXIT_CODE_ARG = -1
+USER_AGENT ={'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'}
+
 
 class DirScan(object):
     """
@@ -19,6 +30,8 @@ class DirScan(object):
     _queue = Queue.Queue()
     _word_list = []
     _thread_list = []
+    _lock = threading.Lock()
+    _user_agent = USER_AGENT
 
     def __init__(self, target, thread_num=5, ext=None, wordlist=None):
         self._target = self._patch_url(target)
@@ -39,27 +52,38 @@ class DirScan(object):
     def _load_dir_dict(self):
         for word_file_path in self._word_list:
             try:
-                with file(word_file_path, 'r') as f:
+                with open(word_file_path, 'r') as f:
                     for line in f:
                         self._queue.put(line)
             except Exception as e:
-                print str(e)
+                print(str(e))
                 sys.exit(-2)
 
 
     def _scan(self):
         while True:
-            print 'threading...'
             if self._queue.empty():
                 break
             try:
-                sub = self._queue.get()
-                target = self._target + sub
+                sub = self._queue.get_nowait()
+                target = self._target + sub.strip()
+                self._lock.acquire()
+                r = requests.get(target, headers=USER_AGENT, timeout=5)
+                code = r.status_code
+                if code == 200:
+                    print('[+][CODE 200] %s' % (target,))
+                if code == 403:
+                    print('[+][CODE 403] %s' % (target,))
+                if code == 304:
+                    print('[+][CODE 304] %s' % (target,))
+                self._lock.release()
             except Exception as e:
-                print str(e)
+                print(str(e))
+            finally:
+                pass
 
     def run(self):
-        for i in xrange(self._thread_num):
+        for i in range(self._thread_num):
             t = threading.Thread(target=self._scan, name=str(i))
             self._thread_list.append(t)
             t.start()
@@ -69,25 +93,25 @@ class DirScan(object):
 
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser('Usage: ')
-    parser.add_option('-t', '--threads', dest='thread_num',
+    opt_parser = optparse.OptionParser('Usage: ')
+    opt_parser.add_option('-t', '--threads', dest='thread_num',
                       default=5, type='int',
                       help='The thread number of program')
-    parser.add_option('-e', '--ext', dest='ext',
+    opt_parser.add_option('-e', '--ext', dest='ext',
                       default=None, type='string',
                       help='The web script extension')
-    parser.add_option('-w', '--wordlist', dest='wordlist', type='string',
+    opt_parser.add_option('-w', '--wordlist', dest='wordlist', type='string',
                       help='The word list path')
 
-    (options, args) = parser.parse_args()
+    (options, args) = opt_parser.parse_args()
     if len(args) < 1 or not options.wordlist:
-        parser.print_help()
+        opt_parser.print_help()
         sys.exit(EXIT_CODE_ARG)
 
     word_list = options.wordlist.split(',')
     word_list = [x for x in word_list if x]
     if len(word_list) < 1:
-        parser.print_help()
+        opt_parser.print_help()
         sys.exit(EXIT_CODE_ARG)
 
     d = DirScan(target=args[0], thread_num=options.thread_num, ext=options.ext, wordlist=word_list)
